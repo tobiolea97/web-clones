0a36ea6d17b012adff18aae57d2c6cd7d20da837
Tobias Olea <tobiolea97@gmail.com>
2024-04-20 11:53:55 -0300

    [Mobile] Submenu toogle working!

--- udemy/scripts.js
+++ udemy/scripts.js
@@ -5,3 +5,14 @@ document.querySelector('.close-sidebar').addEventListener('click', function() {
 document.querySelector('.menu-button').addEventListener('click', function() {
     document.querySelector('.sidebar-section').style.display = 'block';
 });
+
+
+document.querySelector('#submenu-1-btn').addEventListener('click', function() {
+    document.querySelector('#submenu-1').style.display = 'block';
+});
+
+document.querySelector('#close-submenu-1').addEventListener('click', function() {
+    document.querySelector('#submenu-1').style.display = 'none';
+});
+
+
