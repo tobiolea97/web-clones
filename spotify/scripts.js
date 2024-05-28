fc645bf039b73c6f858b741c0a01b9b2da805c75
Tobias Olea <tobiolea97@gmail.com>
2024-05-27 21:37:00 -0300

    Styling show/hide right sidebar button

--- spotify/scripts.js
+++ spotify/scripts.js
@@ -4,6 +4,10 @@ function toogleShowPlayingQueue(flag) {
     document.querySelector('.right-sidebar').style.display = showPlayingQueue ? 'flex' : 'none';
     let pageWrapperClassList = document.querySelector('.page-wrapper').classList;
     showPlayingQueue ? pageWrapperClassList.remove('no-right-sidebar') : pageWrapperClassList.add('no-right-sidebar');
+    document.getElementById('toogleShowPlayingQueueBtn').src = showPlayingQueue ? 'images/play-queue-green.png' : 'images/play-queue-white.png';
+    
+    document.querySelector('#toogleShowPlayingQueueDot').style.visibility = showPlayingQueue ? 'visible' : 'hidden';
+
 }
 
 document.getElementById('toogleShowPlayingQueueBtn')
