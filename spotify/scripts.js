02e56644bc32b2cfa8474c4e39dc16e9ae21a941
Tobias Olea <tobiolea97@gmail.com>
2024-05-27 23:07:05 -0300

    Playing queue/playing info layout switch properly working!

--- spotify/scripts.js
+++ spotify/scripts.js
@@ -1,21 +1,60 @@
-let showPlayingQueue = false;
-function toogleShowPlayingQueue(flag) {
-    showPlayingQueue = flag !== undefined ? flag : !showPlayingQueue;
-    document.querySelector('.right-sidebar').style.display = showPlayingQueue ? 'flex' : 'none';
-    let pageWrapperClassList = document.querySelector('.page-wrapper').classList;
-    showPlayingQueue ? pageWrapperClassList.remove('no-right-sidebar') : pageWrapperClassList.add('no-right-sidebar');
-    document.getElementById('toogleShowPlayingQueueBtn').src = showPlayingQueue ? 'images/play-queue-green.png' : 'images/play-queue-white.png';
-    
-    document.querySelector('#toogleShowPlayingQueueDot').style.visibility = showPlayingQueue ? 'visible' : 'hidden';
-
+let rightSideBarLayout = {
+    showSideBar: false,
+    showPlayingQueue: false,
+    showPlayingInfo: false,
+    playingQueueButtonEnabled: false,
+    playingInfoButtonEnabled: false
 }
 
 document.getElementById('toogleShowPlayingQueueBtn')
         .addEventListener('click', () => {
-            toogleShowPlayingQueue();
+            rightSideBarLayout.showSideBar = !rightSideBarLayout.showPlayingQueue;
+            rightSideBarLayout.showPlayingQueue = !rightSideBarLayout.showPlayingQueue;
+            rightSideBarLayout.showPlayingInfo = false;
+            rightSideBarLayout.playingQueueButtonEnabled = rightSideBarLayout.showPlayingQueue;
+            rightSideBarLayout.playingInfoButtonEnabled = false;
+            configureRightSideBarLayout();
+        });
+
+document.getElementById('toogleShowPlayingInfoBtn')
+        .addEventListener('click', () => {
+            rightSideBarLayout.showSideBar = !rightSideBarLayout.showPlayingInfo;
+            rightSideBarLayout.showPlayingInfo = !rightSideBarLayout.showPlayingInfo;
+            rightSideBarLayout.showPlayingQueue = false;
+            rightSideBarLayout.playingInfoButtonEnabled = rightSideBarLayout.showPlayingInfo;
+            rightSideBarLayout.playingQueueButtonEnabled = false;
+            configureRightSideBarLayout();
         });
 
 document.getElementById('closePlayingQueueBtn')
         .addEventListener('click', () => {
-            toogleShowPlayingQueue(false);
+            rightSideBarLayout.showSideBar = false;
+            rightSideBarLayout.showPlayingQueue = false;
+            rightSideBarLayout.playingQueueButtonEnabled = false;
+            rightSideBarLayout.playingInfoButtonEnabled = false;
+            rightSideBarLayout.showPlayingInfo = false;
+            rightSideBarLayout.showPlayingQueue = false;
+            configureRightSideBarLayout();
         });
+
+function configureRightSideBarLayout() {
+    let rightSideBar = document.querySelector('.right-sidebar');
+    let pageWrapper = document.querySelector('.page-wrapper');
+    let toogleShowPlayingQueueBtn = document.getElementById('toogleShowPlayingQueueBtn');
+    let toogleShowPlayingQueueDot = document.getElementById('toogleShowPlayingQueueDot');
+    let toogleShowPlayingInfoBtn = document.getElementById('toogleShowPlayingInfoBtn');
+    let toogleShowPlayingInfoDot = document.getElementById('toogleShowPlayingInfoDot');
+
+    rightSideBar.style.display = rightSideBarLayout.showSideBar ? 'flex' : 'none';
+    rightSideBarLayout.showSideBar ? pageWrapper.classList.remove('no-right-sidebar'): pageWrapper.classList.add('no-right-sidebar');
+    
+    // playing queue layout
+    toogleShowPlayingQueueBtn.src = rightSideBarLayout.playingQueueButtonEnabled ? 'images/play-queue-green.png' : 'images/play-queue-white.png';
+    toogleShowPlayingQueueDot.style.visibility = rightSideBarLayout.playingQueueButtonEnabled ? 'visible' : 'hidden';
+    
+    // playing info layout
+    toogleShowPlayingInfoBtn.src = rightSideBarLayout.playingInfoButtonEnabled ? 'images/now-playing-green.png' : 'images/now-playing-white.png';
+    toogleShowPlayingInfoDot.style.visibility = rightSideBarLayout.playingInfoButtonEnabled ? 'visible' : 'hidden';
+
+}
+
