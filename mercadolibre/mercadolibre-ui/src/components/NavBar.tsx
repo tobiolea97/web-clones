774e6dee9167990d3469ea9dbc9b79561b08fb96
Tobias Olea <tobiolea97@gmail.com>
2024-08-29 08:54:05 -0300

    Working on mobile layout

--- mercadolibre/mercadolibre-ui/src/components/NavBar.tsx
+++ mercadolibre/mercadolibre-ui/src/components/NavBar.tsx
@@ -1,16 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav className='bg-customYellow'>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="flex bg-customYellow">
+      <div className="h-100 w-48">
+        <img src="./header/logo.png" alt="Logo 25 años" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
