4925b774bbf6907e8c1f49bda5910afb4af6fcfa
tobias.olea <tobiolea97@gmail.com>
2024-11-21 18:18:47 -0300

    Remove Tailwind from project. NavBar cleanning

--- mercadolibre/mercadolibre-ui/src/components/layout/nav-bar/index.tsx
+++ mercadolibre/mercadolibre-ui/src/components/layout/nav-bar/index.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <div className="flex space-between bg-customYellow">
-      <div className="h-100 w-48">
+    <div>
+      <div>
         <img src="./header/logo.png" alt="Logo 25 años" />
       </div>
-      <div className="flex-row space-between bg-customYellow">
+      <div>
         <nav>
           <ul>
             <li>
