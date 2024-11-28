1072758d8bfbe04d856380d1078debc8b140f8c1
tobias.olea <tobiolea97@gmail.com>
2024-11-27 23:45:00 -0300

    Redux toolkit setup for hidding/showing categories/subcategories menu

---
+++ mercadolibre/mercadolibre-ui/src/store/slices/NavBarSlice.tsx
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const NavBarSlice = createSlice({
+  name: "counter", // Nombre del slice
+  initialState: {
+    categoryMenuOpen: false,
+    subcategoyMenuOpen: false,
+  },
+  reducers: {
+    showCategoryMenu: (state) => {
+      state.categoryMenuOpen = true;
+    },
+    hideCategoryMenu: (state) => {
+      state.categoryMenuOpen = false;
+    },
+    showSubcategoryMenu: (state) => {
+      state.subcategoyMenuOpen = true;
+    },
+    hideSubcategoryMenu: (state) => {
+      state.subcategoyMenuOpen = false;
+    },
+  },
+});
+
+export const {
+  showCategoryMenu,
+  hideCategoryMenu,
+  showSubcategoryMenu,
+  hideSubcategoryMenu,
+} = NavBarSlice.actions;
+
+export default NavBarSlice.reducer;
+
